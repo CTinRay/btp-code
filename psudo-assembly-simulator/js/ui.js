df2e9dc26a651abb649b8207a10f45991bcf5600
tinray <b03902072@ntu.edu.tw>
2016-08-06 14:51:33 +0800

    change init val of rom to hlt

--- psudo-assembly-simulator/js/ui.js
+++ psudo-assembly-simulator/js/ui.js
@@ -1,7 +1,7 @@
 var UI = UI || {};
 
 UI.init = function(){
-    let initTable = function(nLines, table, type){
+    let initTable = function(nLines, table, type, init){
         for( let i = 0; i < nLines; ++i ){
             let tr = document.createElement('tr');
             let td1 = document.createElement('td');
@@ -9,13 +9,13 @@ UI.init = function(){
             let td2 = document.createElement('td');
             let cell = document.createElement('input');            
             cell.setAttribute('type', type);
-            cell.value = 0;
+            cell.value = init;
             td2.appendChild(cell);
             tr.appendChild(td1);
             tr.appendChild(td2);
             table.appendChild(tr);
         }
     };
-    initTable(Config.romSize, document.getElementById('rom'), 'text');
-    initTable(Config.ramSize, document.getElementById('ram'), 'number');    
+    initTable(Config.romSize, document.getElementById('rom'), 'text', 'hlt');
+    initTable(Config.ramSize, document.getElementById('ram'), 'number', 0);    
 };
