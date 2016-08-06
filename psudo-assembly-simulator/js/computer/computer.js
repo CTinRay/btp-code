97fb01f7e95c57d9c6bc5663cc2198337033e479
tinray <b03902072@ntu.edu.tw>
2016-08-06 14:18:25 +0800

    add computer cycle

--- psudo-assembly-simulator/js/computer/computer.js
+++ psudo-assembly-simulator/js/computer/computer.js
@@ -4,6 +4,13 @@ Computer.init = function(romId, ramId, pcId){
     Computer.rom = Computer.Memory(romId);
     Computer.ram = Computer.Memory(ramId);
     Computer.pc = document.getElementById(pcId);
+    Computer.pc.value = 0;
 };
 
+Computer.cycle = function(){
+    let cmd = Computer.rom.read(Computer.pc.value).split(' ');
+    let op = cmd[0];
+    let args = cmd.slice(1);
+    Computer.pc.value = Instruction[op].exe(Computer.ram, args);
+};
 
