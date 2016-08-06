4031cf148384330318c3c155796a2ff7179794bc
tinray <b03902072@ntu.edu.tw>
2016-08-06 15:34:05 +0800

    implement instructions

--- psudo-assembly-simulator/js/computer/computer.js
+++ psudo-assembly-simulator/js/computer/computer.js
@@ -8,9 +8,9 @@ Computer.init = function(romId, ramId, pcId){
 };
 
 Computer.cycle = function(){
-    let cmd = Computer.rom.read(Computer.pc.value).split(' ');
+    let cmd = Computer.rom.read(Number(Computer.pc.value)).split(' ');
     let op = cmd[0];
     let args = cmd.slice(1);
-    Computer.pc.value = Instruction[op].exe(Computer.ram, args);
+    Computer.pc.value = Instruction[op].exe(Number(Computer.pc.value), Computer.ram, args);
 };
 
--- psudo-assembly-simulator/js/instruction.js
+++ psudo-assembly-simulator/js/instruction.js
@@ -1,66 +1,132 @@
 var Instruction = {       
     // noop
-    nop: { 
-        nArg: 0
+    hlt: { 
+        nArg: 0,
+        exe: function(pc, ram, args){
+            return pc;
+        }
     },
     // set addr const 
     set: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(args[0], args[1]);
+            return pc + 1;
+        }
     },
     // mov addr addr
     mov: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(args[1], ram.read(args[0]));
+            return pc + 1;
+        }
     },
-    // mov address [address]
+    // mvi address [address]
     mvi: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(args[1], ram.read(ram.read(args[0])));
+            return pc + 1;
+        }        
     },
-    // mov [address] address
+    // imv [address] address
     imv: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(ram.read(args[1]), ram.read(args[0]));
+            return pc + 1;
+        }
     },
-    // adc address const
-    adc: {
-        nArg: 2
-    },
-    // cad const address
-    cad: {
-        nArg: 2
+    // inc address const
+    inc: {
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[0]) + args[1]);
+            return pc + 1;
+        }
     },
     // sht address address
     sht: {
-        nArg: 3
+        nArg: 2,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[0]) << ram.read(args[1]));
+            return pc + 1;
+        }
     },
     // add address address address
     add: {
-        nArg: 3
+        nArg: 3,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[1]) + ram.read(args[2]));
+            return pc + 1;
+        }
     },
     // sub address address address
     sub: {
-        nArg: 3
+        nArg: 3,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[1]) - ram.read(args[2]));
+            return pc + 1;
+        }
     },
     // and address address address
-    and: {
-        nArg: 3
+    and: {        
+        nArg: 3,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[1]) & ram.read(args[2]));
+            return pc + 1;
+        }
     },
     // xor address address address
     xor: {
-        nArg: 3
+        nArg: 3,
+        exe: function(pc, ram, args){
+            ram.write(args[0], ram.read(args[1]) ^ ram.read(args[2]));
+            return pc + 1;
+        }
     },
     // jmp pc
     jmp: {
-        nArg: 1
+        nArg: 1,
+        exe: function(pc, ram, args){
+            return args[0];
+        }
     },
-    // jmp address pc
+    // jeq address pc
     jeq: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            if( ram.read(args[0]) == 0 ){
+                return args[1];
+            }
+            else {
+                return pc + 1;
+            }
+        }
     },
-    // jmp address pc
+    // jgt address pc
     jgt: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            if( ram.read(args[0]) > 0 ){
+                return args[1];
+            }
+            else {
+                return pc + 1;
+            }
+        }
     },
-    // jmp address pc
+    // jlt address pc
     jlt: {
-        nArg: 2
+        nArg: 2,
+        exe: function(pc, ram, args){
+            if( ram.read(args[0]) < 0 ){
+                return args[1];
+            }
+            else {
+                return pc + 1;
+            }
+        }
     }    
 };
