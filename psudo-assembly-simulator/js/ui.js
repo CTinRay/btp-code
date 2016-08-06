78804d5f49e8a9d09946f8b76668ea4bd0fdda9e
tinray <b03902072@ntu.edu.tw>
2016-08-06 19:51:32 +0800

    refine table appearance

--- psudo-assembly-simulator/js/ui.js
+++ psudo-assembly-simulator/js/ui.js
@@ -2,6 +2,7 @@ var UI = UI || {};
 
 UI.init = function(){
     let initTable = function(nLines, table, type, init){
+        let tbody = table.getElementsByTagName('tbody')[0];
         for( let i = 0; i < nLines; ++i ){
             let tr = document.createElement('tr');
             let td1 = document.createElement('td');
@@ -9,11 +10,12 @@ UI.init = function(){
             let td2 = document.createElement('td');
             let cell = document.createElement('input');            
             cell.setAttribute('type', type);
+            cell.setAttribute('class', 'form-control');
             cell.value = init;
             td2.appendChild(cell);
             tr.appendChild(td1);
             tr.appendChild(td2);
-            table.appendChild(tr);
+            tbody.appendChild(tr);
         }
     };
     initTable(Config.romSize, document.getElementById('rom'), 'text', 'hlt');
